de4bc880b431a8fa044cb1a0264371109a290a36
Luthfan Rasyad Maulana <ovan.maulana@gmail.com>
2019-05-10 15:59:06 +1000

    Add files via upload

--- watson.py
+++ watson.py
@@ -1,39 +1,46 @@
 #!/bin/env python
 import json
-import speech_recognition as sr
 from watson_developer_cloud import ToneAnalyzerV3
 
-text = 'I dropped my phone in the toilet yesterday'
+text = "I am very sad"
 
 
 tone_analyzer = ToneAnalyzerV3(
     version='2017-09-21',
-    iam_apikey= [apikey],
-    url='https://gateway-syd.watsonplatform.net/tone-analyzer/api'
-)
+    iam_apikey='BCPnn-DtaBVZEcIU75mDen9uvl8KqrCx7LBUGaIGZ2PQ',
+    url='https://gateway-syd.watsonplatform.net/tone-analyzer/api')
 
-r = sr.Recognizer()
+def detect_emotions(text):
+    emotions = {}
+    tone_analysis = tone_analyzer.tone({'text': text},content_type='application/json').get_result()
+    for i in tone_analysis['document_tone']['tones']:
+       emotions[i['tone_name']] =  i['score']
+    return emotions
+   
+def change_color(emotions):
 
-with sr.Microphone() as source:
-   print('Speak Anything : ')
-   audio = r.listen(source)
+   if 'Fear' in emotions:
+      #pixels.fill((0,255,0))
+      #pixels.show()
+      intensity = (emotions["Fear"]*255)
+      print("Green" , intensity) 
+   elif 'Sadness' in emotions:
+      #pixels.fill((0,0,255))
+      #pixels.show()
+      intensity = (emotions["Sadness"]*255)
+      print("Blue",intensity)
 
-   try:
-      text = r.recognize_google(audio)
-      tone_analysis = tone_analyzer.tone(
-    {'text': text},
-    'application/json'
-).get_result()
-      print('You said : {}'.format(text))
-      
-   except:
-      print("Sorry could not recognize your voice")
+   elif 'Joy' in emotions:
+      #pixels.fill((255,255,0))
+      #pixels.show()
+      intensity = (emotions["Joy"]*255)
+      print("Yellow")
+   elif 'Anger' in emotions:
+      #pixels.fill((255,0,0))
+      #pixels.show()
+      intensity = (emotions["Anger"]*255)
+      print("Red",intensity)
 
-
-print(json.dumps(tone_analysis, indent=2))
-
-
-
-
+change_color(detect_emotions(text))
 
 
